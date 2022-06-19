facfc941157e5cdfaf987a683efb4dea00010b80
gdmen <gmenezes@alumni.upenn.edu>
2022-06-18 19:37:15 -0700

    Add watching_video event reporting.

--- web/src/video.js
+++ web/src/video.js
@@ -1,16 +1,22 @@
-import React, {  useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactPlayer from 'react-player'
 
 import './video.css'
 
 const VideoView = ({ video, postEvent }) => {
-  const [playing, setPlaying] = useState(null);
+  const [playing, setPlaying] = useState(false);
+  const [elapsed, setElapsed] = useState(0);
+
+  const elapsedRef = useRef();
+  useEffect(() => {
+    elapsedRef.current = elapsed;
+  }, [elapsed]);
 
   if (video == null || postEvent == null) {
     return <div id="loading"></div>
   }
 
-  const play = async () => {
+  const play = () => {
     setPlaying(true);
   };
 
@@ -19,6 +25,10 @@ const VideoView = ({ video, postEvent }) => {
       <ReactPlayer
         url={video.url}
         playing={playing}
+        onProgress={(p) => {
+          postEvent("watching_video", p.playedSeconds - elapsedRef.current);
+          setElapsed(p.playedSeconds);
+        }}
         onEnded={() => {
           postEvent("done_watching_video", video.id);
           window.location.href="play";
