1ad904c12af4db1ca1dc4bf7ff1e0788460cef83
gdmen <gmenezes@alumni.upenn.edu>
2022-11-04 11:48:33 -0700

    Add multiplication operation.

--- server/generator/operations.go
+++ server/generator/operations.go
@@ -8,6 +8,7 @@ import (
 const (
 	addDiffMultiplier = 1.0
 	subDiffMultiplier = 1.2
+	mulDiffMultiplier = 2
 )
 
 type Operation struct {
@@ -53,6 +54,19 @@ func sub(a *Problem, b *Problem, opts *Options) *Problem {
 	return prob
 }
 
+func mulInputDiff(maxDiff float64) float64 {
+	return maxDiff / mulDiffMultiplier
+}
+
+func mul(a *Problem, b *Problem, opts *Options) *Problem {
+	prob := &Problem{}
+	prob.Expr = fmt.Sprintf("%s*%s", a.Expr, b.Expr)
+	a_v, b_v := a.GetAns(), b.GetAns()
+	prob.SetAns(a_v.Mul(a_v, b_v))
+	prob.Diff = (a.Diff * b.Diff) * mulDiffMultiplier
+	return prob
+}
+
 var operations = map[string]Operation{
 	"+": {
 		getInputDiff: addInputDiff,
@@ -63,6 +77,11 @@ var operations = map[string]Operation{
 		getInputDiff: subInputDiff,
 		do:           sub,
 		String:       func() string { return "sub" },
+	},
+	"*": {
+		getInputDiff: mulInputDiff,
+		do:           mul,
+		String:       func() string { return "mul" },
 	},
 }
 
