88e78cba6b86e846e5d173cc28ebd6a5aa38b4d5
gdmen <gmenezes@alumni.upenn.edu>
2023-03-01 17:41:06 -0800

    Redesign the settings page.
    And update the /videos API for DELETEs. If a video is in the gamestate,
    it is now replaced if it's deleted.
    
    Fixes #15

--- web/src/settings.js
+++ web/src/settings.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import PinInput from 'react-pin-input';
 
 import { ProblemTypes } from './enums.js'
-import './setup.scss'
+import './settings.scss'
 
 // TODO: clean this file up when I come back to pull views out for the settings page
 
-const ProblemTypesTabView = ({ token, url, user, settings, advanceSettings }) => {
+const ProblemTypesSettingsView = ({ token, url, user, settings }) => {
   const [error, setError] = useState(false);
   const [problemTypeBitmap, setProblemTypeBitmap] = useState(settings.problem_type_bitmap);
 
@@ -32,20 +31,18 @@ const ProblemTypesTabView = ({ token, url, user, settings, advanceSettings }) =>
   const handleCheckboxChange = (e) => {
     let newBitmap = problemTypeBitmap + ((2 * e.target.checked -1) * ProblemTypes[e.target.id]);
     setProblemTypeBitmap(newBitmap);
-    setError(newBitmap < 1);
-  };
-
-  const handleSubmitClick = (e) => {
-    // post updated settings
-    settings.problem_type_bitmap = problemTypeBitmap;
-    postSettings(settings);
-    // redirect to next setup step
-    advanceSettings();
+    let newError = newBitmap < 1;
+    setError(newError);
+    if (!newError) {
+      // post updated settings
+      settings.problem_type_bitmap = newBitmap;
+      postSettings(settings);
+    }
   };
 
   return (<>
-    <h2>Hi there! Let's do a little setup for your kid!</h2>
-    <div className="setup-form">
+    <h2>Settings</h2>
+    <div className="settings-form">
       <h4>Which types of problems should we show? <span className={error ? "error" : ""}>Select one or more.</span></h4>
       <ul id="problem-type-buttons">
         {Object.keys(ProblemTypes).map(function(problemType, i) {
@@ -59,18 +56,17 @@ const ProblemTypesTabView = ({ token, url, user, settings, advanceSettings }) =>
             </li>)
         })}
       </ul>
-      <button className={error ? "submit error" : "submit"} onClick={handleSubmitClick}>continue</button>
     </div>
   </>)
 }
 
-const VideosTabView = ({ token, url, user, advanceSettings }) => {
+const VideosSettingsView = ({ token, url, user }) => {
   const [error, setError] = useState(true);
   const [addError, setAddError] = useState(true);
   const [videos, setVideos] = useState(new Map());
-  const [videoUrl, setVideoUrl] = useState(null);
-  const [videoTitle, setVideoTitle] = useState(null);
-  const [videoThumbnail, setVideoThumbnail] = useState(null);
+  const [videoUrl, setVideoUrl] = useState("");
+  const [videoTitle, setVideoTitle] = useState("");
+  const [videoThumbnail, setVideoThumbnail] = useState("");
 
   useEffect(() => {
     const getVideos = async () => {
@@ -102,11 +98,6 @@ const VideosTabView = ({ token, url, user, advanceSettings }) => {
 
     getVideos();
   }, [token, url, user]);
-
-  const handleSubmitClick = (e) => {
-    // redirect to next setup step
-    advanceSettings();
-  };
 
   const fetchYouTubeMetadata = async function(url, okFcn, errFcn) {
       try {
@@ -155,9 +146,17 @@ const VideosTabView = ({ token, url, user, advanceSettings }) => {
         };
         const req = await fetch(url + "/videos/", settings);
         if (req.ok) {
-          setVideos(videos => new Map(videos.set(video.url, video)));
-          setAddError(true);
+          const json = await req.json();
+          setVideos(videos => new Map(videos.set(json.url, json)));
+          setAddError(false);
           setError(videos.size < 3);
+          if (video.url === videoUrl) {
+            // If the video we just added is currently in the add video UX
+            setVideoUrl("");
+            setVideoTitle(null);
+            setVideoThumbnail(null);
+            setAddError(true);
+          }
         }
       } catch (e) {
         console.log(e.message);
@@ -168,141 +167,81 @@ const VideosTabView = ({ token, url, user, advanceSettings }) => {
     postVideo({"user_id": user.id, "title": videoTitle, "url": videoUrl, "thumbnailurl": videoThumbnail});
   };
 
-  return (<>
-    <div className="setup-form">
-      <h4>Add <span className={error ? "error" : ""}>at least three</span> <a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer">YouTube</a> videos that your kid will love!</h4>
-      <ul id="video-list">
-        {[...videos.keys()].map(function(key, i) {
-            var id = i;
-            var video = videos.get(key);
-            return (<li key={id} style={{
-              backgroundImage: `url(${video.thumbnailurl})` 
-            }}>
-              <span className="video-title">{video.title}</span>
-            </li>)
-        })}
-      </ul>
-      <div id="add-video-interface" style={{
-          backgroundImage: `url(${videoThumbnail})` 
-      }}>
-        <div className={addError ? "video-title error": "video-title"}><h3>{videoTitle}</h3></div>
-        <div id="video-inputs">
-          <input type="text" placeholder="paste a YouTube link here" className={addError && videoUrl ? "error" : ""} onChange={handleAddVideoChange}/>
-          <button className={addError ? "error" : ""} onClick={handleAddVideoClick}>add</button>
-        </div>
-      </div>
-      <button className={error ? "submit error" : "submit"} onClick={handleSubmitClick}>continue</button>
-    </div>
-  </>)
-}
-
-const PinTabView = ({ token, url, user, advanceSettings }) => {
-  const [error, setError] = useState(user.pin.length < 4);
-  const [pin, setPin] = useState(user.pin);
-
-  const handlePinChange = (pin) => {
-    setError(pin.length < 4);
-    if (pin.length === 4) {
-      setPin(pin);
-    }
-  };
-
-  const postUser = async function(user) {
+  const deleteVideo = async function(video) {
       try {
         const settings = {
-            method: 'POST',
+            method: 'DELETE',
             headers: {
               'Accept': 'application/json',
               'Content-Type': 'application/json',
               'Authorization': 'Bearer ' + token,
             },
-            body: JSON.stringify(user),
         };
-        const req = await fetch(url + "/users/" + encodeURIComponent(user.auth0_id), settings);
-        const json = await req.json();
-        return json;
+        const req = await fetch(url + "/videos/" + video.id, settings);
+        if (req.ok) {
+          videos.delete(video.url);
+          if (video.url === videoUrl) {
+            // If we're deleting the video currently in the add video UX
+            setAddError(false);
+          }
+          setError(videos.size < 3);
+          setVideos(new Map(videos));
+        }
       } catch (e) {
         console.log(e.message);
       }
   };
 
-  const handleSubmitClick = (e) => {
-    // post updated settings
-    user.pin = pin;
-    postUser(user);
-    // redirect to next setup step
-    advanceSettings();
+  const handleDeleteVideoClick = (e) => {
+    deleteVideo(videos.get(e.target.getAttribute("data-video-url")));
   };
 
   return (<>
-    <div className="setup-form">
-      <h4>Set a <span className={error ? "error" : ""}>four digit</span> PIN code! You'll need this PIN to edit these settings later!</h4>
-      <PinInput 
-        autoSelect={true}
-        initialValue={pin}
-        inputMode="number"
-        inputStyle={{borderRadius: '0.25em'}}
-        length={4} 
-        onChange={(value, index) => {handlePinChange(value);}}
-        type="numeric"
-      />
-      <button className={error ? "submit error" : "submit"} onClick={handleSubmitClick}>continue</button>
-    </div>
-  </>)
-}
-
-const StartPlayingTabView = () => {
-  return (<>
-    <h2>We're all set!</h2>
-    <div className="setup-form">
-      <h3>The Math Game will start easy and get harder to match your kid's math level!</h3>
-      <button id="start-playing-button" onClick={function(e){window.location.href="play"}}>Start Playing!</button>
+    <div className="settings-form">
+      <h4>Add <span className={error ? "error" : ""}>at least three</span> <a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer">YouTube</a> videos that your kid will love!</h4>
+      <div id="video-inputs">
+        <input type="text" placeholder="paste a YouTube link here" className={addError && videoUrl ? "error" : ""} value={videoUrl} onChange={handleAddVideoChange}/>
+        <button className={addError ? "error" : ""} onClick={handleAddVideoClick}>add</button>
+      </div>
+      <ul id="video-list">
+        <li id="new-video">
+          <span className="video-number"></span>
+          <span className="video-thumbnail" style={{
+            backgroundImage: `url(${videoThumbnail})` 
+          }}></span>
+          <span className="video-title">{videoTitle}</span>
+        </li>
+        <li id="video-list-header">
+          <span className="video-number">#</span>
+          <span className="video-title">TITLE</span>
+        </li>
+        {[...videos.keys()].map(function(key, i) {
+          var id = i+1;
+          var video = videos.get(key);
+          return (<li key={id}>
+            <span className="video-number">{id}</span>
+            <span className="video-thumbnail" style={{
+              backgroundImage: `url(${video.thumbnailurl})` 
+            }}><a className="video-play" href={video.url} target="_blank" rel="noopener noreferrer" >&#9654;</a></span>
+            <span className="video-title">{video.title}</span>
+            <span className={videos.size < 3+1 ? "disabled video-delete" : "video-delete"} data-video-url={video.url} onClick={handleDeleteVideoClick}>x</span>
+          </li>)
+        })}
+      </ul>
     </div>
   </>)
 }
 
 const SettingsView = ({ token, url, user, settings }) => {
-  const [activeTab, setActiveTab] = useState(null);
-
-  const allTabs = ["Choose Problems", "Add Videos", "Set Parent Pin", "Start Playing!"];
-
-  if (activeTab == null) {
-    setActiveTab("Choose Problems");
-  }
-
-  const advanceSettings = function() {
-    setActiveTab(allTabs[allTabs.indexOf(activeTab)+1]);
-  }
-
-  const handleTabClick = (e) => {
-    let clickedId = parseInt(e.target.id.slice(-1));
-    if (clickedId > allTabs.indexOf(activeTab)) {
-      return;
-    }
-    setActiveTab(allTabs[clickedId]);
-  }
-
-  return (<div id="setup">
-    <div id="setup-tabs">
-      {allTabs.map(function(tab, i){
-        var id = "tab" + i;
-        var className = tab === activeTab ? "tab active" : "tab";
-        return (
-          <div key={id} className={className}>
-            <div id={id} className="tab-click-catcher" onClick={handleTabClick}></div>
-            <span className="number">{i+1}</span>
-            <span className="label">{tab}</span>
-          </div>
-        )
-      })}
-    </div>
-    { (activeTab === "Choose Problems") && <div className="tab-content"><ProblemTypesTabView token={token} url={url} user={user} settings={settings} advanceSettings={advanceSettings}/></div> }
-    { (activeTab === "Add Videos") && <div className="tab-content"><VideosTabView token={token} url={url} user={user} advanceSettings={advanceSettings}/></div> }
-    { (activeTab === "Set Parent Pin") && <div className="tab-content"><PinTabView token={token} url={url} user={user} advanceSettings={advanceSettings}/></div> }
-    { (activeTab === "Start Playing!") && <div className="tab-content"><StartPlayingTabView /></div> }
+  return (<div id="settings">
+    <div className="tab-content"><ProblemTypesSettingsView token={token} url={url} user={user} settings={settings} /></div>
+    
+    <div className="tab-content"><VideosSettingsView token={token} url={url} user={user} /></div>
   </div>)
 }
 
 export {
+  ProblemTypesSettingsView,
+  VideosSettingsView,
   SettingsView
 }
