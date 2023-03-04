22e92e515cdb0a551a30d8cbfd43b9eb6364d4c6
gdmen <gmenezes@alumni.upenn.edu>
2023-03-04 11:44:54 -0800

    Fix a migration for older MySQL versions.

--- server/api/migrations/seven.sql
+++ server/api/migrations/seven.sql
@@ -1,6 +1,5 @@
 alter table videos DROP COLUMN start;
 alter table videos DROP COLUMN end;
 alter table videos CHANGE COLUMN thumbnailurl thumbnailurl VARCHAR(256) NOT NULL AFTER url;
-alter table videos RENAME COLUMN enabled TO disabled;
-alter table videos ALTER disabled SET DEFAULT 0;
+alter table videos CHANGE enabled disabled TINYINT NOT NULL DEFAULT 0;
 update videos SET disabled = 0;
