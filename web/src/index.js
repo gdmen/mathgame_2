81ca942580ba5460fb4ce1520b51eca9976d1e2a
gdmen <gmenezes@alumni.upenn.edu>
2023-03-10 14:11:20 -0800

    Disable companion mode for now.
    We can push the work for #73 to after the current milestone.

--- web/src/index.js
+++ web/src/index.js
@@ -12,7 +12,7 @@ import { SetupView } from './setup.js'
 import { PinView, ClearSessionPin } from './pin.js'
 import { SettingsView } from './settings.js'
 import { PlayView } from './play.js'
-import { CompanionView } from './companion.js'
+//import { CompanionView } from './companion.js'
 
 import './index.scss'
 
@@ -58,11 +58,6 @@ const MainView = ({ token, url, isLoading, isAuthenticated, user, settings, post
           <Route exact path="/settings">
             {!isLoading && isAuthenticated &&
               <SettingsView token={token} url={url} user={user} settings={settings} />
-            }
-          </Route>
-          <Route exact path="/companion/:student_id">
-            {!isLoading && isAuthenticated &&
-              <CompanionView token={token} url={url} user={user} />
             }
           </Route>
           <Route path="*" component={NotFound} />
