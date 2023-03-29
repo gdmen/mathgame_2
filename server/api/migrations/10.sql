92b9196e2e1898568846b5c1fa410d3f334fc088
gdmen <gmenezes@alumni.upenn.edu>
2023-03-29 16:22:04 -0700

    Fix typo in migration.

--- server/api/migrations/10.sql
+++ server/api/migrations/10.sql
@@ -1,4 +1,4 @@
 ALTER TABLE settings ALTER problem_type_bitmap DROP DEFAULT;
 ALTER TABLE settings ALTER target_difficulty DROP DEFAULT;
 ALTER TABLE settings ALTER target_work_percentage DROP DEFAULT;
-ALTER TABLE gamestate ALTER target DROP DEFAULT;
+ALTER TABLE gamestates ALTER target DROP DEFAULT;
