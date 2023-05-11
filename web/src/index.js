gdmen/mathgame_2

commit d01c8718859a92279157208a745a423ea9a8d711

Move pin from options to user model.
part of #45

web/src/index.js
@@ -30,7 +30,8 @@ const MainView = ({ token, url, isLoading, isAuthenticated, user, options, postE
     )
   }
   // TODO: setup if: (pin not set OR no operations set OR no videos set)
-  else if (options != null && options.pin === "") {
+  else if (options != null && user.pin === "") {
+    console.log(user);
     return <SetupView token={token} url={url} user={user} options={options}/>
   }
   else {
@@ -133,19 +134,14 @@ const AppView = () => {
           return;
         }
         const settings = {
-            method: 'POST',
+            method: 'GET',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + token,
             },
-            body: JSON.stringify({
-              auth0_id: user.sub,
-              email: user.email,
-              username: user.name,
-            })
         };
-        const req = await fetch(ApiUrl + "/users", settings);
+        const req = await fetch(ApiUrl + "/users/" + encodeURIComponent(user.sub), settings);
         const json = await req.json();
         setAppUser(json);
         genPostEventFcn("logged_in", "");

server/api/migrations/three.sql
@@ -1,0 +1,2 @@
+ALTER TABLE users ADD pin VARCHAR(4) NOT NULL DEFAULT '';
+ALTER TABLE options DROP COLUMN pin;

server/api/option_model.go
@@ -15,8 +15,7 @@ const (
 	operations VARCHAR(256) NOT NULL DEFAULT '+',
 	fractions TINYINT NOT NULL DEFAULT 0,
 	negatives TINYINT NOT NULL DEFAULT 0,
-	target_difficulty DOUBLE NOT NULL DEFAULT 3,
-	pin VARCHAR(4) NOT NULL DEFAULT ''
+	target_difficulty DOUBLE NOT NULL DEFAULT 3
     ) DEFAULT CHARSET=utf8 ;`
 
 	createOptionSQL = `INSERT INTO options (user_id) VALUES (?);`
@@ -27,7 +26,7 @@ const (
 
 	listOptionSQL = `SELECT * FROM options;`
 
-	updateOptionSQL = `UPDATE options SET operations=?, fractions=?, negatives=?, target_difficulty=?, pin=? WHERE user_id=?;`
+	updateOptionSQL = `UPDATE options SET operations=?, fractions=?, negatives=?, target_difficulty=? WHERE user_id=?;`
 
 	deleteOptionSQL = `DELETE FROM options WHERE user_id=?;`
 )
@@ -38,11 +37,10 @@ type Option struct {
 	Fractions        bool    `json:"fractions" uri:"fractions" form:"fractions"`
 	Negatives        bool    `json:"negatives" uri:"negatives" form:"negatives"`
 	TargetDifficulty float64 `json:"target_difficulty" uri:"target_difficulty" form:"target_difficulty"`
-	Pin              string  `json:"pin" uri:"pin" form:"pin"`
 }
 
 func (model Option) String() string {
-	return fmt.Sprintf("UserId: %v, Operations: %v, Fractions: %v, Negatives: %v, TargetDifficulty: %v, Pin: %v", model.UserId, model.Operations, model.Fractions, model.Negatives, model.TargetDifficulty, model.Pin)
+	return fmt.Sprintf("UserId: %v, Operations: %v, Fractions: %v, Negatives: %v, TargetDifficulty: %v", model.UserId, model.Operations, model.Fractions, model.Negatives, model.TargetDifficulty)
 }
 
 type OptionManager struct {
@@ -66,7 +64,7 @@ func (m *OptionManager) Create(model *Option) (int, string, error) {
 
 func (m *OptionManager) Get(user_id uint32) (*Option, int, string, error) {
 	model := &Option{}
-	err := m.DB.QueryRow(getOptionSQL, user_id).Scan(&model.UserId, &model.Operations, &model.Fractions, &model.Negatives, &model.TargetDifficulty, &model.Pin)
+	err := m.DB.QueryRow(getOptionSQL, user_id).Scan(&model.UserId, &model.Operations, &model.Fractions, &model.Negatives, &model.TargetDifficulty)
 	if err == sql.ErrNoRows {
 		msg := "Couldn't find a option with that user_id"
 		return nil, http.StatusNotFound, msg, err
@@ -88,7 +86,7 @@ func (m *OptionManager) List() (*[]Option, int, string, error) {
 	}
 	for rows.Next() {
 		model := Option{}
-		err = rows.Scan(&model.UserId, &model.Operations, &model.Fractions, &model.Negatives, &model.TargetDifficulty, &model.Pin)
+		err = rows.Scan(&model.UserId, &model.Operations, &model.Fractions, &model.Negatives, &model.TargetDifficulty)
 		if err != nil {
 			msg := "Couldn't scan row from database"
 			return nil, http.StatusInternalServerError, msg, err
@@ -114,7 +112,7 @@ func (m *OptionManager) CustomList(sql string) (*[]Option, int, string, error) {
 	}
 	for rows.Next() {
 		model := Option{}
-		err = rows.Scan(&model.UserId, &model.Operations, &model.Fractions, &model.Negatives, &model.TargetDifficulty, &model.Pin)
+		err = rows.Scan(&model.UserId, &model.Operations, &model.Fractions, &model.Negatives, &model.TargetDifficulty)
 		if err != nil {
 			msg := "Couldn't scan row from database"
 			return nil, http.StatusInternalServerError, msg, err
@@ -136,7 +134,7 @@ func (m *OptionManager) Update(model *Option) (int, string, error) {
 		return status, msg, err
 	}
 	// Update
-	_, err = m.DB.Exec(updateOptionSQL, model.Operations, model.Fractions, model.Negatives, model.TargetDifficulty, model.Pin, model.UserId)
+	_, err = m.DB.Exec(updateOptionSQL, model.Operations, model.Fractions, model.Negatives, model.TargetDifficulty, model.UserId)
 	if err != nil {
 		msg := "Couldn't update option in database"
 		return http.StatusInternalServerError, msg, err

server/api/user_model.go
@@ -14,7 +14,8 @@ const (
         auth0_id VARCHAR(225) NOT NULL PRIMARY KEY,
 	id BIGINT UNSIGNED AUTO_INCREMENT UNIQUE,
 	email VARCHAR(320) NOT NULL,
-	username VARCHAR(128) NOT NULL
+	username VARCHAR(128) NOT NULL,
+	pin VARCHAR(4) NOT NULL DEFAULT ''
     ) DEFAULT CHARSET=utf8 ;`
 
 	createUserSQL = `INSERT INTO users (auth0_id, email, username) VALUES (?, ?, ?);`
@@ -25,7 +26,7 @@ const (
 
 	listUserSQL = `SELECT * FROM users;`
 
-	updateUserSQL = `UPDATE users SET id=?, email=?, username=? WHERE auth0_id=?;`
+	updateUserSQL = `UPDATE users SET id=?, email=?, username=?, pin=? WHERE auth0_id=?;`
 
 	deleteUserSQL = `DELETE FROM users WHERE auth0_id=?;`
 )
@@ -35,10 +36,11 @@ type User struct {
 	Id       uint32 `json:"id" uri:"id" form:"id"`
 	Email    string `json:"email" uri:"email" form:"email"`
 	Username string `json:"username" uri:"username" form:"username"`
+	Pin      string `json:"pin" uri:"pin" form:"pin"`
 }
 
 func (model User) String() string {
-	return fmt.Sprintf("Auth0Id: %v, Id: %v, Email: %v, Username: %v", model.Auth0Id, model.Id, model.Email, model.Username)
+	return fmt.Sprintf("Auth0Id: %v, Id: %v, Email: %v, Username: %v, Pin: %v", model.Auth0Id, model.Id, model.Email, model.Username, model.Pin)
 }
 
 type UserManager struct {
@@ -69,7 +71,7 @@ func (m *UserManager) Create(model *User) (int, string, error) {
 
 func (m *UserManager) Get(auth0_id string) (*User, int, string, error) {
 	model := &User{}
-	err := m.DB.QueryRow(getUserSQL, auth0_id).Scan(&model.Auth0Id, &model.Id, &model.Email, &model.Username)
+	err := m.DB.QueryRow(getUserSQL, auth0_id).Scan(&model.Auth0Id, &model.Id, &model.Email, &model.Username, &model.Pin)
 	if err == sql.ErrNoRows {
 		msg := "Couldn't find a user with that auth0_id"
 		return nil, http.StatusNotFound, msg, err
@@ -91,7 +93,7 @@ func (m *UserManager) List() (*[]User, int, string, error) {
 	}
 	for rows.Next() {
 		model := User{}
-		err = rows.Scan(&model.Auth0Id, &model.Id, &model.Email, &model.Username)
+		err = rows.Scan(&model.Auth0Id, &model.Id, &model.Email, &model.Username, &model.Pin)
 		if err != nil {
 			msg := "Couldn't scan row from database"
 			return nil, http.StatusInternalServerError, msg, err
@@ -117,7 +119,7 @@ func (m *UserManager) CustomList(sql string) (*[]User, int, string, error) {
 	}
 	for rows.Next() {
 		model := User{}
-		err = rows.Scan(&model.Auth0Id, &model.Id, &model.Email, &model.Username)
+		err = rows.Scan(&model.Auth0Id, &model.Id, &model.Email, &model.Username, &model.Pin)
 		if err != nil {
 			msg := "Couldn't scan row from database"
 			return nil, http.StatusInternalServerError, msg, err
@@ -139,7 +141,7 @@ func (m *UserManager) Update(model *User) (int, string, error) {
 		return status, msg, err
 	}
 	// Update
-	_, err = m.DB.Exec(updateUserSQL, model.Id, model.Email, model.Username, model.Auth0Id)
+	_, err = m.DB.Exec(updateUserSQL, model.Id, model.Email, model.Username, model.Pin, model.Auth0Id)
 	if err != nil {
 		msg := "Couldn't update user in database"
 		return http.StatusInternalServerError, msg, err

web/src/setup.js
@@ -5,7 +5,7 @@ import './setup.scss'
 
 // TODO: clean this file up when I come back to pull views out for the settings page
 
-const OperationsTabView = ({ token, url, user, options, postOptions, advanceSetup }) => {
+const OperationsTabView = ({ token, url, user, options, advanceSetup }) => {
   const allOperationsMap = new Map([
     ["Addition", "+"],
     ["Subtraction", "-"],
@@ -16,6 +16,25 @@ const OperationsTabView = ({ token, url, user, options, postOptions, advanceSetu
   ]);
   const [error, setError] = useState(false);
   const [operations, setOperations] = useState(options.operations.split(",").map(function(op, i) { return revAllOperationsMap.get(op); }));
+
+  const postOptions = async function(options) {
+      try {
+        const settings = {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + token,
+            },
+            body: JSON.stringify(options),
+        };
+        const req = await fetch(url + "/options/" + options.user_id, settings);
+        const json = await req.json();
+        return json;
+      } catch (e) {
+        console.log(e.message);
+      }
+  };
 
   const handleCheckboxChange = (e) => {
     let id = e.target.id;
@@ -191,9 +210,9 @@ const VideosTabView = ({ token, url, user, advanceSetup }) => {
   </>)
 }
 
-const PinTabView = ({ token, url, user, options, postOptions, advanceSetup }) => {
-  const [error, setError] = useState(options.pin.length < 4);
-  const [pin, setPin] = useState(options.pin);
+const PinTabView = ({ token, url, user, advanceSetup }) => {
+  const [error, setError] = useState(user.pin.length < 4);
+  const [pin, setPin] = useState(user.pin);
 
   const handlePinChange = (pin) => {
     setError(pin.length < 4);
@@ -202,10 +221,29 @@ const PinTabView = ({ token, url, user, options, postOptions, advanceSetup }) =>
     }
   };
 
+  const postUser = async function(user) {
+      try {
+        const settings = {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + token,
+            },
+            body: JSON.stringify(user),
+        };
+        const req = await fetch(url + "/users/" + encodeURIComponent(user.auth0_id), settings);
+        const json = await req.json();
+        return json;
+      } catch (e) {
+        console.log(e.message);
+      }
+  };
+
   const handleSubmitClick = (e) => {
     // post updated options
-    options.pin = pin;
-    postOptions(options);
+    user.pin = pin;
+    postUser(user);
     // redirect to next setup step
     advanceSetup();
   };
@@ -258,25 +296,6 @@ const SetupView = ({ token, url, user, options }) => {
     setActiveTab(allTabs[clickedId]);
   }
 
-  const postOptions = async function(options) {
-      try {
-        const settings = {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-              'Authorization': 'Bearer ' + token,
-            },
-            body: JSON.stringify(options),
-        };
-        const req = await fetch(url + "/options/" + options.user_id, settings);
-        const json = await req.json();
-        return json;
-      } catch (e) {
-        console.log(e.message);
-      }
-  };
-
   return (<div id="setup">
     <div id="setup-tabs">
       {allTabs.map(function(tab, i){
@@ -291,9 +310,9 @@ const SetupView = ({ token, url, user, options }) => {
         )
       })}
     </div>
-    { (activeTab === "Choose Operations") && <div className="tab-content"><OperationsTabView token={token} url={url} user={user} options={options} postOptions={postOptions} advanceSetup={advanceSetup}/></div> }
+    { (activeTab === "Choose Operations") && <div className="tab-content"><OperationsTabView token={token} url={url} user={user} options={options} advanceSetup={advanceSetup}/></div> }
     { (activeTab === "Add Videos") && <div className="tab-content"><VideosTabView token={token} url={url} user={user} advanceSetup={advanceSetup}/></div> }
-    { (activeTab === "Set Parent Pin") && <div className="tab-content"><PinTabView token={token} url={url} user={user} options={options} postOptions={postOptions} advanceSetup={advanceSetup}/></div> }
+    { (activeTab === "Set Parent Pin") && <div className="tab-content"><PinTabView token={token} url={url} user={user} advanceSetup={advanceSetup}/></div> }
     { (activeTab === "Start Playing!") && <div className="tab-content"><StartPlayingTabView /></div> }
   </div>)
 }

commit 7f3fe6d9be04cb3c4ffc329703594dcef8b2c484

Fix signup bug.
This was broken in d01c8718859a92279157208a745a423ea9a8d711
New users were never being added to our database.

web/src/index.js
@@ -170,10 +170,30 @@ const AppView = () => {
             Authorization: "Bearer " + token,
           },
         };
-        const req = await fetch(
+        var req = await fetch(
           ApiUrl + "/users/" + encodeURIComponent(user.sub),
           reqParams
         );
+        if (req.status === 404) {
+          // Looking up the auth0 user failed, so create this user in our database.
+          const reqParams = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token,
+            },
+            body: JSON.stringify({
+              auth0_id: user.sub,
+              email: user.email,
+              username: user.name,
+            })
+          };
+          req = await fetch(
+            ApiUrl + "/users",
+            reqParams
+          );
+        }
         const json = await req.json();
         setAppUser(json);
         genPostEventFcn("logged_in", "");