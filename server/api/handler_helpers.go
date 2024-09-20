d3a2e89f87f2f51762aed60b7a612f377a234ac0
gdmen <gmenezes@alumni.upenn.edu>
2024-09-19 21:34:28 -0700

    Reset last answer when new problems are displayed.
    Fixes #25

--- server/api/handler_helpers.go
+++ server/api/handler_helpers.go
@@ -69,7 +69,7 @@ func OptionalWriteHandleMngrResp(logPrefix string, c *gin.Context, status int, m
 
 	glog.Infof("%s (HTTP %d) %T: %+v", logPrefix, status, model, model)
 	if writeModel {
-		glog.Infof("%s writing to reponse body: %v", logPrefix, model)
+		glog.Infof("%s writing to response body: %v", logPrefix, model)
 		c.JSON(status, model)
 	}
 	return nil
